cce3d88751a1f3072cf1b396eb150138c4afe64c
Mario Ramos <marioramosmontesinos@gmail.com>
2022-10-16 12:50:17 +0200

    Main page WIP

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,15 @@
             <v-avatar
               rounded="0"
               class="mr-3"
-              image="https://cdn.vuetifyjs.com/docs/images/logos/v.png"
+              image="ServerlessTwitterBotLogo.png"
             />
 
-            Vuetify 3
+            Serverless Twitter Bot
           </div>
         </v-app-bar-title>
-<!--        <v-spacer></v-spacer>
-        <v-btn :icon="true" @click="toggleTheme"><v-icon>mdi-theme-light-dark</v-icon></v-btn>-->
+        <v-spacer></v-spacer>
+        <v-btn icon="mdi-twitter" color="whitesmoke" href="https://twitter.com/GigachadBot_" target="_blank"></v-btn>
+<!--        <v-btn :icon="true" @click="toggleTheme"><v-icon>mdi-theme-light-dark</v-icon></v-btn>-->
       </v-container>
     </v-app-bar>
 
@@ -25,15 +26,22 @@
           color="grey-darken-3"
         >
           <v-container class="text-center">
-            <v-responsive class="mx-auto" width="500">
-              <v-img src="https://cdn.vuetifyjs.com/docs/images/logos/v.png"></v-img>
+            <v-responsive class="mx-auto" width="600">
+              <v-img src="/ServerlessTwitterBotLogo.png" ></v-img>
 
               <h3 class="text-h3">
-                Try the new Vuetify
+                Serverless Twitter Bot
               </h3>
 
-              <p class="mt-4 text-medium-emphasis">
+<!--              <p class="mt-4 text-medium-emphasis">
                 This is a basic Vuetify 3 application designed to help get your feet wet with the next version of the framework. Visit our <a href="https://next.vuetifyjs.com/" target="_blank" rel="noopener noreferrer">documentation</a> for more information.
+              </p>-->
+
+              <p class="mt-4 text-medium-emphasis">
+                 This is a serverless Twitter bot that tweets a random sentence from a list of sentences
+              </p>
+              <p class="mt-4 text-medium-emphasis">
+                Hosted in Azure using Functions, CosmosDB, KeyVault and Vue
               </p>
             </v-responsive>
           </v-container>
@@ -45,49 +53,92 @@
           <v-container>
             <v-row justify="space-between">
               <v-col cols="auto">
-                <v-responsive width="350">
+                <v-responsive width="550">
                   <h2 class="text-h4">
-                    New customization system
+                    Send a new sentence
                   </h2>
 
-                  <p class="text-success mt-3">
-                    Global Defaults
-                  </p>
-
                   <strong class="mt-3">
-                    Version 3 Only
+                    You are also part of this!
                   </strong>
 
                   <p class="mt-8">
-                    Vuetify 3 has an unprecedented level of customization options that make implementing any design system easy.
-                  </p>
+                    Submit your preferred phrase so that (after we validate it) it becomes part of the bot's database                  </p>
 
-                  <p class="mt-8">
-                    Assign default values for all components in the library, including nested support.
-                  </p>
+                  <v-textarea :loading="loading" class="mt-6" label="Sentence" variant="outlined" counter :rules="rules"
+                              v-model="value" rows="3" auto-grow></v-textarea>
 
                   <v-btn
-                    class="mt-6"
-                    href="https://next.vuetifyjs.com/features/global-configuration/"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                    prepend-icon="mdi-send"
+                    :loading="loading"
                   >
-                    More Information
+                    Send
                   </v-btn>
                 </v-responsive>
               </v-col>
 
               <v-img
                 max-width="400"
-                src="https://cdn.vuetifyjs.com/store/themes/vite-free/chips-bar.png"
+                max-height="300"
+                src="gigachad.jpg"
               />
             </v-row>
           </v-container>
         </section>
 
-<!--        <v-container>
+        <v-container>
           <v-divider class="my-12" />
-        </v-container>-->
+        </v-container>
+
+        <section id="github">
+          <v-container>
+            <v-row justify="space-between">
+              <v-col cols="auto">
+                <v-responsive width="550">
+                  <h2 class="text-h4">
+                    Take a look at the code
+                  </h2>
+
+                  <strong class="mt-3">
+                    Don't be shy, you can contribute!
+                  </strong>
+
+                  <p class="mt-8">
+                    The backend consists of a set of Azure Functions, in a solution using Clean Architecture, CQRS and MediatR
+                  </p>
+
+                  <v-btn
+                    class="mt-2"
+                    prepend-icon="mdi-server"
+                    href="https://github.com/MarioRamosEs/ServerlessCommunityTwitterBot"
+                    target="_blank"
+                  >
+                    BackEnd Code
+                  </v-btn>
+
+                  <p class="mt-8">
+                    The frontend is a Vue 3 application, using Vuetify 3
+                  </p>
+
+                  <v-btn
+                    class="mt-2"
+                    prepend-icon="mdi-web"
+                    href="https://github.com/MarioRamosEs/ServerlessCommunityTwitterBot"
+                    target="_blank"
+                  >
+                    FrontEnd Code
+                  </v-btn>
+                </v-responsive>
+              </v-col>
+
+              <v-img
+                max-width="400"
+                max-height="300"
+                src="Octocat.png"
+              />
+            </v-row>
+          </v-container>
+        </section>
 
       </v-sheet>
 
@@ -101,7 +152,7 @@
         </div>
         <div>
           Made with
-          <v-btn variant="text" href="https://next.vuetifyjs.com/" target="_blank" icon="mdi-vuetify" size="x-large" color="white">
+          <v-btn variant="text" href="https://next.vuetifyjs.com/" target="_blank" icon="mdi-vuetify"  color="whitesmoke">
           </v-btn>
         </div>
       </v-container>
@@ -120,6 +171,12 @@
         toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
       }
     }*/
+
+    data: () => ({
+      rules: [v => v.length <= 280 || 'Max 280 characters'],
+      value: '',
+      loading: false
+    }),
   }
 </script>
 
