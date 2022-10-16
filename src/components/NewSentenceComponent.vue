d6f8088618740b09c72dce0f9ac398655b3d9b38
Mario Ramos <marioramosmontesinos@gmail.com>
2022-10-16 15:33:44 +0200

    Update NewSentenceComponent.vue

--- src/components/NewSentenceComponent.vue
+++ src/components/NewSentenceComponent.vue
@@ -12,33 +12,84 @@
             becomes part of the bot's database
           </p>
 
-          <v-textarea
-            :loading="loading"
-            class="mt-6"
-            label="Sentence"
-            variant="outlined"
-            counter
-            :rules="rules"
-            v-model="value"
-            rows="3"
-            auto-grow
-          ></v-textarea>
+          <v-form v-model="valid" class="mt-6">
+            <v-textarea
+              :loading="loading"
+              label="Sentence"
+              variant="outlined"
+              counter
+              :rules="rules"
+              v-model="newSentence"
+              rows="3"
+              auto-grow
+            ></v-textarea>
 
-          <v-btn prepend-icon="mdi-send" :loading="loading"> Send </v-btn>
+            <v-btn
+              prepend-icon="mdi-send"
+              :loading="loading"
+              @click="sendSentence"
+            >
+              Send
+            </v-btn>
+          </v-form>
+
         </v-responsive>
       </v-col>
 
       <v-img max-width="400" max-height="300" src="gigachad.jpg" />
     </v-row>
   </v-container>
+
+  <v-snackbar
+    v-model="snackbar"
+    color="primary"
+    :timeout="2000"
+  >
+    {{ text }}
+    <template v-slot:action="{ attrs }">
+    </template>
+  </v-snackbar>
+
 </template>
 <script>
 export default {
   name: "NewSentenceComponent",
   data: () => ({
     rules: [(v) => v.length <= 280 || "Max 280 characters"],
-    value: "",
+    newSentence: "",
     loading: false,
+    valid: false,
+    snackbar: false,
+    text: ''
   }),
+  methods: {
+    async sendSentence() {
+      if (!this.valid || this.newSentence.length === 0) {
+        return;
+      }
+
+      console.log('hey')
+      this.loading = true;
+      try {
+        const response = await fetch(
+          "https://communitytwitterbot.azurewebsites.net/api/CreatePendingSentence?code=KA4yB-TU9pZiggBY-pzXhBGICnvFnmSncwcJ9pGcsW0JAzFuPDoy9w==",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ text: this.newSentence }),
+          }
+        );
+        this.text = "Your sentence has been sent to the bot!";
+        this.snackbar = true;
+      } catch (e) {
+        console.error(e);
+        this.text = "There was an error sending your sentence";
+        this.snackbar = true;
+      }
+      this.loading = false;
+    },
+  },
 };
 </script>
